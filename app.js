94a3b1134cd2563c2ca78eb723aceca3b853ba99
Zachary Gordon <z.gordon.h@gmail.com>
2016-07-15 11:48:45 -0700

    Create grayed overlay with click event

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 
+var j_overlay = $('<div id="overlay"></div>');
+$("body").append(j_overlay);
 
-
+// Creating a click event
+$("#imageGallery a").click(function(event) {
+  event.preventDefault();
+  var href = $(this).attr("href");
+  console.log(href);
+  j_overlay.show();
+});
 
