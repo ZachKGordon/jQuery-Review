b2a70be37f19de087695863014297a0cc5106848
Zachary Gordon <z.gordon.h@gmail.com>
2016-07-15 12:25:17 -0700

    Show and hide image overlay

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 
-var j_overlay = $('<div id="overlay"></div>');
-$("body").append(j_overlay);
+//Building the overlay
+var $overlay = $('<div id="overlay"></div>');
+$('body').append($overlay);
+
+var $image = $('<img>');
+$overlay.append($image);
 
 // Creating a click event
-$("#imageGallery a").click(function(event) {
+$('#imageGallery a').click(function(event) {
   event.preventDefault();
-  var href = $(this).attr("href");
-  console.log(href);
-  j_overlay.show();
+  var href = $(this).attr('href');
+  $image.attr('src',href);
+  $overlay.show();
 });
 
+//Exit from picture
+$overlay.click(function(){
+  $overlay.hide();
+});
