8bf60f96ae6a6d050af934c55f8d627bbec5ad95
Zachary Gordon <z.gordon.h@gmail.com>
2016-08-01 16:12:42 -0700

    Create caption for each image when clicked

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ $('body').append($overlay);
 var $image = $('<img>');
 $overlay.append($image);
 
+var $caption = $("<p></p>");
+$overlay.append($caption);
+
 // Creating a click event
 $('#imageGallery a').click(function(event) {
   event.preventDefault();
   var href = $(this).attr('href');
   $image.attr('src',href);
   $overlay.show();
+
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
 });
 
 //Exit from picture
